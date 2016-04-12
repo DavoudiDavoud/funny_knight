4a8e607f26ce261e75b57883807e1656c4e5a65a
DavoudiDavoud <davoudi.davoud@yahoo.com>
2016-04-12 23:37:55 +0100

    Update window.h

--- window.h
+++ window.h
@@ -35,7 +35,6 @@ private:
 	QwtThermo    *thermo;
 	QwtPlot      *plot;
 	QwtPlotCurve *curve;
-	QLabel       *label;
 	// layout elements from Qt itself http://qt-project.org/doc/qt-4.8/classes.html
 	QVBoxLayout  *vLayout;  // vertical layout
 	QHBoxLayout  *hLayout;  // horizontal layout
