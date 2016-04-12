2f2d2992f937906dc712a422d89413ee13d7c8cf
DavoudiDavoud <davoudi.davoud@yahoo.com>
2016-04-13 00:56:56 +0100

    Update window.cpp

--- window.cpp
+++ window.cpp
@@ -81,7 +81,7 @@ Window::~Window() {
 void Window::timerEvent( QTimerEvent * )
 {
 	inVal = gain * sin( M_PI * count/50.0 );
-	++count;}
+	++count;
 	// add the new input to the plot
 	memmove( yData, yData+1, (plotDataSize-1) * sizeof(double) );
 	yData[plotDataSize-1] = inVal;
