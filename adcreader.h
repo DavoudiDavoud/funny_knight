782b08d72461d870d36227ec3cee8c0c106b222e
DavoudiDavoud <davoudi.davoud@yahoo.com>
2016-04-13 03:53:22 +0100

    Update adcreader.h

--- adcreader.h
+++ adcreader.h
@@ -7,15 +7,7 @@ public:
 	ADCreader();
 	void quit();
 	void run();
-	int ret;
-	int fd;
-	int sysfs_fd;
-	int no_tty;
-	
-	static const char *device = "/dev/spidev0.0";
-	static uint8_t mode = SPI_CPHA | SPI_CPOL;;
-	static uint8_t bits = 8;
-	static int drdy_GPIO = 22;
+
 
 private:
 	bool running;
