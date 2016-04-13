0a0f15e145a876ff9237a07f57a3c9e198f37777
DavoudiDavoud <davoudi.davoud@yahoo.com>
2016-04-13 03:47:14 +0100

    Update adcreader.h

--- adcreader.h
+++ adcreader.h
@@ -1,14 +1,35 @@
 #ifndef ADCREADER
 #define ADCREADER
 
-#include <QThread>
+
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
+
+#include "gz_clk.h"
+#include "gpio-sysfs.h"
 
 class ADCreader : public QThread
 {
 public:
-	ADCreader() {running = 0;};
+	ADCreader();
 	void quit();
 	void run();
+	int ret;
+	int fd;
+	int sysfs_fd;
+	int no_tty;
+	
+	static const char *device = "/dev/spidev0.0";
+	static uint8_t mode = SPI_CPHA | SPI_CPOL;;
+	static uint8_t bits = 8;
+	static int drdy_GPIO = 22;
+
 private:
 	bool running;
 
